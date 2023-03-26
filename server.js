bb3c88d49da6a7c115d8020235e8da77e1e7f414
Atif Hussain <atifhussain28@gmail.com>
2023-03-27 00:46:41 +0530

    Created static frontend using html and css

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express=require('express')
+const app=express()
+const http=require('http').createServer(app)
+
+const PORT=process.env.PORT||3000
+
+http.listen(PORT,()=>{
+    console.log(`Backend server is running on ${PORT}`);
+})
+
+app.use(express.static(__dirname+'/client'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/client/index.html')
+})
